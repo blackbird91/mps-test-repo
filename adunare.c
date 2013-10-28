2221acf3dabde0822ea49110fb4cf06a70a043e3
dbsima <dragos.sima@cti.pub.ro>
2013-10-28 17:16:18 +0200

    test

--- adunare.c
+++ adunare.c
@@ -1,6 +1,21 @@
 #include <stdion.h>
 
 int main()
-{
-return ;
+
+	int c, d, 
+	for ( i = 0 ; i < m ; i++ )
+      		for ( j = 0 ; j < n ; j++ )
+         		sum[i][j] = first[i][j] + second[i][j];
+ 
+   printf("Sum of entered matrices:-\n");
+ 
+   for ( c = 0 ; c < m ; c++ )
+   {
+   	for ( d = 0 ; d < n ; d++ )
+        	printf("%d\t", sum[c][d]);
+
+      printf("\n");
+   }
+
+return 0;
 }
